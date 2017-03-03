59cc678044a61685d99ad9789cd895a961e2f839
hub3rt <hubert.girondiac@gmail.com>
2017-03-03 10:27:07 +0100

    Add btn

--- player.cpp
+++ player.cpp
@@ -14,8 +14,20 @@ Player::~Player()
 
 void Player::init()
 {
-    setAttribute(Qt::WA_TranslucentBackground);
-    setWindowFlags(Qt::FramelessWindowHint);
+//    setAttribute(Qt::WA_TranslucentBackground);
+//    setWindowFlags(Qt::FramelessWindowHint);
+    m_mediaPlayer = new QMediaPlayer;
+    m_startPauseBtn = new QPushButton();
+    m_stopBtn = new QPushButton();
+    m_nextBtn = new QPushButton();
+    m_prevBtn = new QPushButton();
+    m_chooseFileBtn = new QPushButton();
+    m_reduceBtn = new QPushButton();
+    m_exitBtn = new QPushButton();
+
+
+
+
 }
 
 void Player::handle()
